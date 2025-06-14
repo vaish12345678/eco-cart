111401d8f167284142506b03881a0413146470b5
Vaishnavi Ambhore <vaishnaviambhore2006@gmail.com>
2025-06-14 17:44:10 +0530

    Authentication feature added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-
 import './App.css'
+import Navbar from "./components/Navbar"
+import Login from "./components/Auth/Login"
+import Signup from "./components/Auth/Signup"
+import Home from "./components/Home"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element :<Home/>
+
+  }, 
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+
+  },
+  
+]);
 
 function App() {
   
 
   return (
     <>
-   <h1 className="text-4xl font-bold text-green-600">EcoCart</h1>
+  
+     <RouterProvider router={appRouter}/>
      
     </>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Toaster } from "sonner";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <Toaster richColors position="top-right" />
     <App />
   </StrictMode>,
 )
